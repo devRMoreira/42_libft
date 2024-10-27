71579bdd77dbfc10137b03519dc6671d098df76e
RM <devrmoreira@gmail.com>
2024-10-27 18:11:16 +0000

    substr

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: rimagalh <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/24 19:36:38 by rimagalh          #+#    #+#             */
-/*   Updated: 2024/10/27 16:16:32 by rimagalh         ###   ########.fr       */
+/*   Updated: 2024/10/27 18:11:03 by rimagalh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,20 @@
 
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
+	size_t	i;
+	char	*sub;
+
+	i = 0;
+	if (!s)
+		return (NULL);
+	sub = malloc(sizeof(char) * (((int)len - start) + 1));
+	if (!sub)
+		return (NULL);
+	while (i < len)
+	{
+		sub[i] = s[i];
+		i++;
+	}
+	sub[i] = '\0';
+	return (sub);
 }
-// Parameters s: The string from which to create the substring.
-// start: The start index of the substring in the
-// string ’s’.
-// len: The maximum length of the substring.
-// Return value The substring.
-// NULL if the allocation fails.
-// External functs. malloc
-// Description Allocates (with malloc(3)) and returns a substring
-// from the string ’s’.
-// The substring begins at index ’start’ and is of
-// maximum size ’len’
