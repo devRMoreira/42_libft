e13a774f27faeb861c8e1487e9e2c4bc19160438
RM <devrmoreira@gmail.com>
2024-10-27 22:11:53 +0000

    strlcat fix

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: rimagalh <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/24 15:42:13 by rimagalh          #+#    #+#             */
-/*   Updated: 2024/10/27 17:38:09 by rimagalh         ###   ########.fr       */
+/*   Updated: 2024/10/27 22:11:14 by rimagalh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,24 +14,23 @@
 
 size_t	ft_strlcat(char *dst, const char *src, size_t size)
 {
-	unsigned int	i;
-	unsigned int	dstlen;
-	unsigned int	srclen;
+	size_t	i;
+	size_t	j;
+	size_t	dstlen;
+	size_t	srclen;
 
-	if (!src || !dst)
-		return (0);
-	i = 0;
-	srclen = (size_t) ft_strlen(src);
-	dstlen = (size_t) ft_strlen(dst);
-	if (size < dstlen)
-		return (size);
-	while (i < (size - dstlen) - 1)
+	dstlen = ft_strlen(dst);
+	srclen = ft_strlen(src);
+	i = dstlen;
+	j = 0;
+	if (size <= dstlen)
+		return (size + srclen);
+	while (src[j] != '\0' && i < size - 1)
 	{
-		dst[i + dstlen] = src[i];
+		dst[i] = src[j];
 		i++;
+		j++;
 	}
-	if (src[i] != '\0')
-		return (size);
-	dst[i + dstlen] = '\0';
+	dst[i] = '\0';
 	return (dstlen + srclen);
 }
