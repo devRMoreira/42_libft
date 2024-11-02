d2ce50e9e9747238592f34b57746ab5d037a3c97
Ricardo M. <devrmoreira@gmail.com>
2024-11-02 22:41:54 +0000

    Refactor ft_memmove to improve code clarity and remove commented-out code

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: rimagalh <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/17 18:31:41 by rimagalh          #+#    #+#             */
-/*   Updated: 2024/11/02 21:28:40 by rimagalh         ###   ########.fr       */
+/*   Updated: 2024/11/02 21:37:47 by rimagalh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,14 +22,13 @@ void	*ft_memmove(void *dest, const void *src, size_t n)
 		return (NULL);
 	dest1 = (unsigned char *)dest;
 	src1 = (const unsigned char *)src;
-
 	if (dest > src)
 	{
 		i = n;
 		while (i-- > 0)
 			dest1[i] = src1[i];
-
-	} else
+	}
+	else
 	{
 		i = 0;
 		while (i < n)
@@ -40,28 +39,3 @@ void	*ft_memmove(void *dest, const void *src, size_t n)
 	}
 	return (dest);
 }
-
-	// unsigned const char	*src1;
-	// unsigned char		*dest1;
-	// size_t				i;
-
-	// if (!dest && !src)
-	// 	return (NULL);
-	// dest1 = (unsigned char *)dest;
-	// src1 = (unsigned char *)src;
-	// if (src1 < dest1 && dest1 < src1 + n)
-	// {
-	// 	i = n;
-	// 	while (i-- > 0)
-	// 		dest1[i] = src1[i];
-	// }
-	// else
-	// {
-	// 	i = 0;
-	// 	while (i < n)
-	// 	{
-	// 		dest1[i] = src1[i];
-	// 		i++;
-	// 	}
-	// }
-	// return (dest);
