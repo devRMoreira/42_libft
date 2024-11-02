4d37a7ba2bd8a9ddbd303cfbc0cf2ea0bab4bfd0
Ricardo M. <devrmoreira@gmail.com>
2024-11-02 22:41:36 +0000

    Refactor ft_strlcpy to improve handling of size parameter and update return value

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: rimagalh <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/24 17:22:15 by rimagalh          #+#    #+#             */
-/*   Updated: 2024/11/02 15:49:30 by rimagalh         ###   ########.fr       */
+/*   Updated: 2024/11/02 22:40:51 by rimagalh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,18 @@
 
 size_t	ft_strlcpy(char *dst, const char *src, size_t size)
 {
+	size_t	srclen;
 	size_t	i;
 
 	i = 0;
-	while (i < size - 1)
+	srclen = ft_strlen(src);
+	if (size <= 0)
+		return (srclen);
+	while (i < size - 1 && src[i] != '\0')
 	{
 		dst[i] = src[i];
 		i++;
 	}
 	dst[i] = '\0';
-	return (ft_strlen(src));
+	return (srclen);
 }
