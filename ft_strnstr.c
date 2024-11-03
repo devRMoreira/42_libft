8e2cc810222725ca370b05fcc887621d94dfdd66
Ricardo M. <devrmoreira@gmail.com>
2024-11-03 01:29:32 +0000

    refactor and fix

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: rimagalh <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/24 17:26:53 by rimagalh          #+#    #+#             */
-/*   Updated: 2024/11/03 01:19:30 by rimagalh         ###   ########.fr       */
+/*   Updated: 2024/11/03 01:29:02 by rimagalh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,20 +17,19 @@ char	*ft_strnstr(const char *big, const char *little, size_t len)
 	size_t	i;
 	size_t	j;
 
-
-	if (!little)
+	if (little[0] == '\0')
 		return ((char *)big);
 	i = 0;
-	j = 0;
 	while(big[i] != '\0' && i < len)
 	{
+		j = 0;
 		while(big[i + j] == little[j] && little[j] != '\0'
-		&& big[i + j] != '\0' && i + j < len)
+		&& big[i + j] != '\0' && (i + j) < len)
+		{
 			j++;
 		if(little[j] == '\0')
 			return ((char *)big + i);
-		else
-			j = 0;
+		}
 		i++;
 	}
 	return (NULL);
