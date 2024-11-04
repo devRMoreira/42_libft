89ad6de3cb3b6b0fda774145480f58d7fe36ff32
RM <devrmoreira@gmail.com>
2024-11-04 12:58:26 +0000

    fixes

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: rimagalh <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/17 16:32:26 by rimagalh          #+#    #+#             */
-/*   Updated: 2024/11/02 15:33:31 by rimagalh         ###   ########.fr       */
+/*   Updated: 2024/11/04 12:49:20 by rimagalh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,11 +19,11 @@ char	*ft_strchr(char *str, int c)
 	i = 0;
 	while (str[i] != '\0')
 	{
-		if (str[i] == c)
+		if (str[i] == (unsigned char) c)
 			return (str + i);
 		i++;
 	}
-	if (str[i] == c)
+	if (str[i] == (unsigned char) c)
 		return (str + i);
 	return (NULL);
 }
--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: rimagalh <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/17 16:44:03 by rimagalh          #+#    #+#             */
-/*   Updated: 2024/10/28 18:46:08 by rimagalh         ###   ########.fr       */
+/*   Updated: 2024/11/04 12:49:43 by rimagalh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,11 +19,11 @@ char	*ft_strrchr(char *str, int c)
 	i = 0;
 	while (str[i] != '\0')
 		i++;
-	if (str[i] == c)
+	if (str[i] == (unsigned char) c)
 		return (str + i);
 	while (i >= 0)
 	{
-		if (str[i] == c)
+		if (str[i] == (unsigned char) c)
 			return (str + i);
 		i--;
 	}
