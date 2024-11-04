47defe1620e7c72c900e8e915017f37c2e026158
RM <devrmoreira@gmail.com>
2024-11-04 13:02:28 +0000

    refactor ft_strnstr and ft_substr for improved readability and consistency

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: rimagalh <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/24 17:26:53 by rimagalh          #+#    #+#             */
-/*   Updated: 2024/11/03 01:29:02 by rimagalh         ###   ########.fr       */
+/*   Updated: 2024/11/04 13:02:18 by rimagalh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,15 +20,15 @@ char	*ft_strnstr(const char *big, const char *little, size_t len)
 	if (little[0] == '\0')
 		return ((char *)big);
 	i = 0;
-	while(big[i] != '\0' && i < len)
+	while (big[i] != '\0' && i < len)
 	{
 		j = 0;
-		while(big[i + j] == little[j] && little[j] != '\0'
-		&& big[i + j] != '\0' && (i + j) < len)
+		while (big[i + j] == little[j] && little[j] != '\0'
+			&& big[i + j] != '\0' && (i + j) < len)
 		{
 			j++;
-		if(little[j] == '\0')
-			return ((char *)big + i);
+			if (little[j] == '\0')
+				return ((char *)big + i);
 		}
 		i++;
 	}
--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: rimagalh <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/24 19:36:38 by rimagalh          #+#    #+#             */
-/*   Updated: 2024/11/04 12:57:16 by rimagalh         ###   ########.fr       */
+/*   Updated: 2024/11/04 13:01:47 by rimagalh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,16 +28,13 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 		sub[0] = '\0';
 		return (sub);
 	}
-	if(start + len > ft_strlen(s))
+	if (start + len > ft_strlen(s))
 		len = ft_strlen(s) - start;
 	sub = malloc(sizeof(char) * (len + 1));
 	if (!sub)
 		return (NULL);
 	while (i < len)
-	{
-		sub[i] = s[i + start];
-		i++;
-	}
+		sub[i++] = s[start++];
 	sub[i] = '\0';
 	return (sub);
 }
